de82e728bedceae8ae5bb54d1299721a56bd72d4
James Kratus <jimkratus@gmail.com>
2018-09-04 13:10:06 -0400

    fizzbuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public void fizzBuzz(int number) {
+        if((number % 15) == 0) {
+            System.out.println("FizzBuzz");
+        } else if((number % 3) == 0) {
+            System.out.println("Fizz");
+        } else if ((number % 5) == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
+    }
+}
